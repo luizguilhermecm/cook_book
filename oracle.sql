f6f1ebde5c050836c165159d9938a2ab0be1d007
Luiz Guilherme Castilho Martins <luizgui@gmail.com>
2015-11-26 18:17:49 -0200

    Update oracle.sql

--- oracle.sql
+++ oracle.sql
@@ -4,3 +4,15 @@ SELECT owner, table_name
   WHERE table_name like 'DED%' 
   AND owner = 'SADA' 
   ORDER BY table_name
+
+
+-- select all columns of tables with a name and owner
+SELECT table_name || ' : ' || column_name
+FROM   all_tab_cols
+where table_name like 'DED172%'
+and owner = 'SADA'
+order by 1
+
+-- limit of postgres in oracle
+SELECT * FROM table_name WHERE rownum < 20;
+
