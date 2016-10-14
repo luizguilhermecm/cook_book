75c8614fa2ae32c0e7e74fc884d9e7e5d56514e3
Luiz Guilherme Castilho Martins <luizgui@gmail.com>
2016-10-14 17:16:26 -0300

    Update clone.java

--- jaguar/clone.java
+++ jaguar/clone.java
@@ -1,1 +1,10 @@
 
+class {
+	@Inject @QPlcDefault 
+	protected PlcBeanCloneUtil beanCloneUtil;
+	
+method {
+FruicaoFeriasEntity fruicaoClone = null;
+fruicaoClone = (FruicaoFeriasEntity) beanCloneUtil.cloneBean(hafe.getFruicaoFerias());
+}
+}
